5a8d19753a4f3b6a94e83afb88c0a17b6e914265
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-09 12:34:52 -0300

    Home, Products and Product form page finished

--- vendor/forms.py
+++ vendor/forms.py
@@ -6,6 +6,13 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name', 'required': True}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'placeholder': 'Enter a description'}),
+            'price': forms.TextInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter a price', 'required': True}),
+            'stock': forms.TextInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter stock', 'required': True}),
+        }
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
