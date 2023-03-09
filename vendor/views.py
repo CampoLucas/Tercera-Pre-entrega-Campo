42720747dbf3a9dab1b37e8337ff14adec2564ed
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-09 10:50:54 -0300

    Added empty htmls as place holders

--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from vendor.models import Product, ProductOrder, Store
+from vendor.forms import ProductForm, ProductOrderForm, StoreForm
 
-# Create your views here.
+
