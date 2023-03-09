6d62f2c62db8b2f3a36cc068b0ceefa2ec1f0790
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-09 10:46:20 -0300

    Store, Product and ProductOrder models added

--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveBigIntegerField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='products', blank=True, null=True)
+
+class ProductOrder(models.Model):
+    shiping_address = models.TimeField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='oredered_product')
+    quantity = models.PositiveBigIntegerField()
