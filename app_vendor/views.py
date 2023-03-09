9053dc8da8bd457f8429fe9901422c348b238246
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-09 10:29:16 -0300

    Revert "Products page and add product page"
    This reverts commit fb4095b2224265ae2032f626010c75cbd90948fd.

--- app_vendor/views.py
+++ app_vendor/views.py
@@ -1,38 +1,3 @@
 from django.shortcuts import render
-from app_vendor.models import Product, Order, Store
-from app_vendor.forms import ProductForm, OrderForm, StoreForm, OrderProductForm
 
-def home(request):
-    return render(request, "app_vendor/home.html")
-
-def show_products(request):
-    context = {
-        "products": Product.objects.all(), 
-    }
-    return render(request, "app_vendor/product.html", context)
-
-def add_product(request):
-    if request.POST:
-        product_form = ProductForm(request.POST)
-        product_form.save()
-    context = {
-        "form": ProductForm(),
-    }
-    return render(request, "app_vendor/product_form.html", context)
-
-def add_product_form(request):
-    context = {
-        "form": ProductForm()
-    }
-
-def show_orders(request):
-    context = {
-        "orders": Order.objects.all(), 
-    }
-    return render(request, "app_vendor/order.html", context)
-
-def add_order(request):
-    pass
-
-def show_stores(request):
-    pass
+# Create your views here.
