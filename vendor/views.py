2c09e1c31fb4c93d855bc5528baaa8439649b38a
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-09 10:56:38 -0300

    Added the show and add functions for the models in view

--- vendor/views.py
+++ vendor/views.py
@@ -2,4 +2,50 @@ from django.shortcuts import render
 from vendor.models import Product, ProductOrder, Store
 from vendor.forms import ProductForm, ProductOrderForm, StoreForm
 
+def home(request):
+    return render(request, "vendor/home.html")
 
+def show_products(request):
+    context = {
+        "products": Product.objects.all(), 
+    }
+    return render(request, "vendor/products.html", context)
+
+def add_product(request):
+    if request.POST:
+        product_form = ProductForm(request.POST)
+        product_form.save()
+    context = {
+        "form": ProductForm(),
+    }
+    return render(request, "vendor/product_form.html", context)
+
+def show_orders(request):
+    context = {
+        "orders": ProductOrder.objects.all(), 
+    }
+    return render(request, "vendor/orders.html", context)
+
+def add_order(request):
+    if request.POST:
+        order_form = ProductOrderForm(request.POST)
+        order_form.save()
+    context = {
+        "form": ProductOrderForm(),
+    }
+    return render(request, "vendor/order_form.html", context)
+
+def show_stores(request):
+    context = {
+        "stores": Store.objects.all(), 
+    }
+    return render(request, "vendor/stores.html", context)
+
+def add_store(request):
+    if request.POST:
+        store_form = StoreForm(request.POST)
+        store_form.save()
+    context = {
+        "form": StoreForm(),
+    }
+    return render(request, "vendor/store_form.html", context)
