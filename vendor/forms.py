1d76c3c84ed5433562502d49e954b05cffbec1e9
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-10 19:30:55 -0300

    Orders page and forms are functional

--- vendor/forms.py
+++ vendor/forms.py
@@ -9,8 +9,8 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name', 'required': True}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'placeholder': 'Enter a description'}),
-            'price': forms.TextInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter a price', 'required': True}),
-            'stock': forms.TextInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter stock', 'required': True}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter a price', 'required': True}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter stock', 'required': True}),
         }
 
 class OrderForm(forms.ModelForm):
@@ -18,7 +18,18 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = '__all__'
 
+        widgets = {
+            'shiping_address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter shipping address', 'required': True}),
+            'product': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a product', 'required': True}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control', 'type': 'number', 'placeholder': 'Enter stock', 'required': True}),
+        }
+
 class StoreForm(forms.ModelForm):
     class Meta:
         model = Store
         fields = '__all__'
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name', 'required': True}),
+            'location': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter store location', 'required': True}),
+        }
